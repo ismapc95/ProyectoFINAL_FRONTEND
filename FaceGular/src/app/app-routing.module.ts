6f803e07087df87c3de862cdb1268e687a5ef455
ismapc95 <ismapc2@gmail.com>
2019-11-18 16:34:25 +0100

    Creating components

--- FaceGular/src/app/app-routing.module.ts
+++ FaceGular/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistorietasComponent } from './historietas/historietas.component';
+import { CosasComponent } from './cosas/cosas.component';
+import { ColegasComponent } from './colegas/colegas.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HistorietasComponent},
+  { path: 'things', component: CosasComponent},
+  { path: 'friends', component: ColegasComponent},
+  { path: 'events', component: EventosComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FaceGular/src/app/app.module.ts
+++ FaceGular/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistorietasComponent } from './historietas/historietas.component';
+import { CosasComponent } from './cosas/cosas.component';
+import { ColegasComponent } from './colegas/colegas.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistorietasComponent,
+    CosasComponent,
+    ColegasComponent,
+    EventosComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
